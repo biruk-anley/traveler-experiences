892c4f643c7f4a87a3af3ca37842c16281554102
biruk_anley <birukanley2022@gmail.com>
2022-12-31 22:54:38 +0300

    accessing files from temprary data

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import ted from '../images/ted.jpg'
-import { FaBeer } from "react-icons/fa";
+import ted from '../images/one.jpg'
+import { FaSearchLocation } from "react-icons/fa";
 
 
-const  main=()=> {
+const  main=({content})=> {
   return (
     <div className='main'>
       <div className='left'>
-        <img src={ted} />
+        <img src={`../images/${content.img}`} />
       </div>
       
       <div className='right'>
-        <FaBeer />
-        <h2>Tewodros</h2>
-        <p>View on Google Map</p>
-        <h2>Emperor Tewodros</h2>
-        <h4> 12,Jan2022 - 34 Dec 2011</h4>
+        <span>
+          <FaSearchLocation />  
+        </span>
+        <span>
+          View on Google Map
+          
+        </span>
+        <h2>{ content.headertitle}</h2>
+        <h4> {content.date}</h4>
         <p>
-          Emperor Tewodros is one of an Infulential king who
-          make ethiopian great during 18th.C he is the king that
-          change the style for governance of Ethiopia.
+          {content.description}
         </p>
       </div>
       
