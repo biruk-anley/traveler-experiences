2a3d0c5ccc6b353a0207ef80d2edd6cc3743cf16
biruk_anley <birukanley2022@gmail.com>
2022-12-31 13:46:31 +0300

    update header

---
+++ src/component/Headers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import nav from '../images/fill213.png'
+
+const  header=()=> {
+  return (
+
+    <div className='nav'>
+      <img src={nav} />
+      <h2>
+          My Travel Journal
+        </h2>
+    </div>
+  )
+}
+
+export default header;
