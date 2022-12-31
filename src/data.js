3f043ef7015606a9a628ea40564695df293fcaa0
biruk_anley <birukanley2022@gmail.com>
2022-12-31 22:53:20 +0300

    add data.js to store the data temporarily

--- src/data.js
+++ src/data.js
@@ -1,6 +1,17 @@
 export default [
-    {},
-    {},
-    {},
-    {}
+    {
+        img: "one.jpg",
+        headertitle: "እንሳሮ",
+        date:"4,June 2012 - 25,june45",
+        description:"It is a place where you can find caves and nice view of ethiopian. In addtion, if you had gone in summer you will get water falls",
+        
+    },
+    {
+        img: "two.jpg",
+        headertitle: "ደብረ ታቦር መድሃኒአለም",
+        date:"4,June 2012 - 25,june45",
+        description:"it is one of ethiopian orthodox church which is found in deberetabor which has build by King tewodros and it has also ancient historical heritages",
+        
+    }
+    
 ]
