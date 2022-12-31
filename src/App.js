9999fa8f59257bd0df242bb734013566c0310722
biruk_anley <birukanley2022@gmail.com>
2022-12-31 22:52:55 +0300

    update app.js to access database

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import head from './images/fill213.png';
 import './App.css';
 import Headers from './component/Headers';
 import Main from './component/Main';
+import two from './images/two.jpg';
+import data from './data'
+
 
 
 function App() {
+  const traveles = data.map(content => {
+    return <Main
+      content={content}
+    />
+  });
   return (
     <div className="App">
       <Headers />
-      <Main/>
+      {traveles}
       
     </div>
   );
